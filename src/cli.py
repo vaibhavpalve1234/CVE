3798a15a50a92a6faff634052945f70fd4f32cf4
v.palve777 <vaibhavpalve1234@gmail.com>
2026-04-28 21:40:09 +0530

    done

--- src/cli.py
+++ src/cli.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import argparse
 import json
 from src.rag_pipeline import CVERag
--- src/config.py
+++ src/config.py
@@ -2,7 +2,9 @@ DATA_PATH = "data/processed/cve_dataset.json"
 FAISS_INDEX_PATH = "data/embeddings/faiss.index"
 
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-SLM_MODEL = "models/slm"   # after LoRA training
+# SLM_MODEL = "models/slm" 
+SLM_MODEL = "distilgpt2"  # or local TinyLlama path
+
 
 TOP_K = 5
 MAX_TOKENS = 200
